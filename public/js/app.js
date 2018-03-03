c2a46c1de63e62e1203409418a081cc1d1d1067f
gabrielamf <glennygabb@gmail.com>
2018-03-02 19:32:16 -0500

    funcion data mercado libre

--- public/js/app.js
+++ public/js/app.js
@@ -1,75 +1,46 @@
 
-var $productsDisplay = $('.card');
-
 $.ajax({
-  url: 'https://api.mercadolibre.com/sites/MPE/search?q=juegosyjuguetes'
-  
-}).done(handleSuccess)
-  .fail(handleError);
-
-function handleSuccess(data) {
-  console.log(data);
-  console.log(data.results.length);
-  console.log(data.results[0].title);
-  console.log(data.results[0].price);
-  output = '';
-  for (i = 0; i < data.results.length; i++) {
-    console.log(data.results[i].title);
-    console.log(data.results[i].price);
-    console.log(data.results[i].thumbnail);
-    var productName = data.results[i].title;
-    var productPrice = data.results[i].price;
-    var productImage = data.results[i].thumbnail;
-    output += `
-    <img class="card-img-top img-fluid" src="${productImage}" alt="product image">
-    <div class="card-body">
-      <h5 class="card-title">${productName}</h5>
-      <p class="card-text">Precio: ${productPrice} soles</p>
-      <a href="#" class="btn btn-success">ver producto</a>
-    </div>
-    `;
-    $productsDisplay.append(output);
+  url: 'https://api.mercadolibre.com/categories/MPE1132', // cambiar el
+  success: function(data) {
+    // console.log(data.children_categories);
+    var item = data.children_categories;
+    console.log(item);
+    createItem(item);
   }
-  
-};
+});
 
-function handleError() {
-  console.log('error');
-};
+function createItem(arr) {
+  arr.forEach(function(el) {
+    var name = el.name;
+    var id = el.id;
+    var template = `<li class="subcategoria" data-id='${id}'>${name}</li>`;
+    $('#container').append(template);
+  });
+}
 
-// boton de paypal
-
-paypal.Button.render({
-  env: 'production', // Or 'sandbox',
-
-  commit: true, // Show a 'Pay Now' button
-
-  style: {
-    color: 'gold',
-    size: 'small'
-  },
-
-  payment: function(data, actions) {
-    /* 
-     * Set up the payment here 
-     */
-  },
-
-  onAuthorize: function(data, actions) {
-    /* 
-     * Execute the payment here 
-     */
-  },
-
-  onCancel: function(data, actions) {
-    /* 
-     * Buyer cancelled the payment 
-     */
-  },
-
-  onError: function(err) {
-    /* 
-     * An error occurred during the transaction 
-     */
+$(document).on('click', '.subcategoria', function() { 
+  console.log('funcionando');
+  var id = $(this).data('id');
+  console.log(id);
+  datosItem(id);
+  function datosItem(dates) {
+    console.log(dates);
+    $.ajax({
+      url: 'https://api.mercadolibre.com/sites/MPE/search?category=' + dates + '',
+      success: function(data) {
+        console.log(data.results);
+        var data = data.results;
+        $('#container-items').html('');
+        data.forEach(function(el) {
+          var title = el.title;
+          var imagen = el.thumbnail;
+          var price = el.price;
+          var template = `<div><p>${title}</p><img src="${imagen}"><p>s/. ${price}</p></div>`;
+          $('#container-items').append(template);
+        });
+      }
+    });
   }
-}, '#paypal-button');
+});
+
+
