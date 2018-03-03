30741ee5026e51d175e0991ba9926aabfc4308bf
lucerohospina <lucerojhp@gmail.com>
2018-03-03 08:54:40 -0500

    aplicando estilos bootstrap4, uniformiza declaracion de variables

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,5 @@
+let subcategories = $('#subcategories-container');
+let itemsContainer = $('#items-container');
 
 $.ajax({
   url: 'https://api.mercadolibre.com/categories/MPE1132', // cambiar el
@@ -13,30 +15,39 @@ function createItem(arr) {
   arr.forEach(function(el) {
     var name = el.name;
     var id = el.id;
-    var template = `<li class="subcategoria" data-id='${id}'>${name}</li>`;
-    $('#container').append(template);
+    var template = `<li class="subcategory" data-id='${id}'>${name}</li>`;
+    subcategories.append(template);
   });
 }
 
-$(document).on('click', '.subcategoria', function() { 
+$(document).on('click', '.subcategory', function() { 
   console.log('funcionando');
   var id = $(this).data('id');
   console.log(id);
   datosItem(id);
-  function datosItem(dates) {
-    console.log(dates);
+
+  function datosItem(id) {
+    console.log(id);
     $.ajax({
-      url: 'https://api.mercadolibre.com/sites/MPE/search?category=' + dates + '',
+      url: 'https://api.mercadolibre.com/sites/MPE/search?category=' + id + '',
       success: function(data) {
         console.log(data.results);
         var data = data.results;
-        $('#container-items').html('');
+        itemsContainer.html('');
         data.forEach(function(el) {
           var title = el.title;
-          var imagen = el.thumbnail;
+          var image = el.thumbnail;
           var price = el.price;
-          var template = `<div><p>${title}</p><img src="${imagen}"><p>s/. ${price}</p></div>`;
-          $('#container-items').append(template);
+          var template = `
+          <div class="card">
+          <img class="card-img-top img-fluid" src="${image}" alt="item-image">
+          <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text">s/. ${price}</p>
+          </div>
+          </div>
+          `;
+          itemsContainer.append(template);
         });
       }
     });
